8ed2c7624aec8af6418eff98b814be8d4c8e9875
苍穹勇敢的心 <18825237023@163.com>
2019-10-22 16:23:08 +0800

    菜单和路由优化

--- react-demo/react组件demo/src/App.js
+++ react-demo/react组件demo/src/App.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-29 16:33:56
- * @LastEditTime: 2019-10-21 21:22:43
+ * @LastEditTime: 2019-10-22 15:31:34
  * @LastEditors: Please set LastEditors
  */
 import React,{Component} from 'react';
@@ -13,13 +13,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch, NavLink, Redirect } from "react-router-dom";
 
 import Login from './components/Login';
-// 公共头部组件
-import Header from './components/Header'
  // 404页面
  import NotFound from './components/NotFound'
 // 引入路由
 import routes from './routerMap';
 
+// 引入store状态--->toggleSideBar
+import {toggleSideBar} from './components/store'
 
 const isLogined = localStorage.getItem('userInfo');
 //如果已经登录，不拦截，否则跳转登录页面
@@ -28,45 +28,89 @@ class Routers extends Component {
   constructor(props){
     super(props)
     this.state ={
-      navList:[]
+      navList:[],
+      date: new Date(),
+      toggleState: toggleSideBar.getState()
     }
-  
+    this.toggleSide =this.toggleSide.bind(this)
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  //   我们会在 componentWillUnmount() 生命周期方法中清除计时器
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  toggleSide(){
+    if(toggleSideBar.getState()){
+      toggleSideBar.dispatch({type: 'hide'})
+    }else {
+      toggleSideBar.dispatch({type: 'open'})
+    }
+    this.setState({
+      toggleState: toggleSideBar.getState()
+    })
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
   }
   
   render() { 
     return ( 
-      <div className="App">
-      {
-      <Router>
-        <div className="left">     {/* 左边路由区域 */}
-          <header className="App-header">      
-              <ul className="nav el-menu">
-                {routes.map((v,i)=>
-                      <li key={i} className="el-menu-item"><NavLink to={v.path} >{v.name}</NavLink></li>
-                )}
-                </ul>
-          </header>
-        </div>
-        <div className="right">    {/* 右边context */}
-          <div className="navbar">
-            <Header/>
-          </div>
-          <div className="content">
-          <Switch>
-              {
-                routes.map((v,i)=>
-                    <Route key={i} path={v.path} component={v.component} />
-                    
-                )
-                // <Route path='/login' component={Login} />
-              }
-              <Redirect from="/*" to="/404" />
-              </Switch>
-          </div>
-        </div>
-      </Router>
-      }
+      <div className="App app-wrapper">
+        <div className={this.state.toggleState ?'openSideBar' : 'hideSideBar'}>
+          {
+          <Router>
+            <div className="left">     {/* 左边路由区域 */}
+              <header className="App-header">      
+                  <ul className="nav el-menu">
+                    {routes.map((v,i)=>
+                      <li key={i}>
+                        {
+                          v.children ? v.children.map((item,index)=>
+                            <NavLink className="el-menu2" key={index} to={item.path} >{item.name}</NavLink>
+                          ) 
+                          : <NavLink className="el-menu1" to={v.path} >{v.name}</NavLink>
+                        }
+                      </li>
+                    )}
+                    </ul>
+              </header>
+            </div>
+            <div className="right">    {/* 右边context */}
+              <div className="navbar">
+                <div className="the_header">
+                  <button className="btn btn-primary" onClick={this.toggleSide}>切换</button>
+                  <span className="the_clock">当前时间：{this.state.date.toLocaleTimeString()}.</span>
+                </div>
+              </div>
+              <div className="content">
+                <Switch>
+                  {
+                    routes.map((v,i)=>
+                        <Route key={i} exact path={v.path}
+                          render={
+                            props=>v.auth ? (<v.component {...props}/>) : (<Redirect to="/login" />)
+                          }
+                        />
+                        
+                    )
+                  }
+                  {/* <Redirect from="/*" to="/home" /> */}
+                  {/* // 所有错误路由跳转页面 */}
+                <Route component={NotFound} />
+                </Switch>
+              </div>
+            </div>
+          </Router>
+          }
       
+      </div>
     </div>
      );
   }
--- react-demo/react组件demo/src/routerMap.js
+++ react-demo/react组件demo/src/routerMap.js
@@ -2,23 +2,25 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-10-21 19:52:42
- * @LastEditTime: 2019-10-21 21:24:10
+ * @LastEditTime: 2019-10-22 16:22:00
  * @LastEditors: Please set LastEditors
  */
 // 引入组件----------NotFound---
 import Login from './components/Login';
 import User from './components/User';
 import Home from './components/Home';
-import Xiaojiejie from './components/Xiaojiejie';
-import Boss from './components/Boss';
 import TodoList from './components/TodoList';
 
 
+
 export default [
-  {path: "/home",component: Home,name:'主页'},
-  {path: "/login",component: Login,name:'登录页'},
-  { path: "/user",component: User,name:'用户一览页'},
-  {path: "/xiaojie", component: Xiaojiejie,name:'Xiaojiejie'},
-  {path: "/boss", component: Boss,name:'Boss'},
-  {path: "/todoList", component: TodoList,name:'TodoList'},
+  {path: "/home",component: Home,name:'主页', auth:true},
+  {path: "/login",component: Login,name:'登录页', auth:true},
+  {path: "/user",component: User,name:'用户一览页',auth:true},
+  {path: "/todoList/", component: TodoList,name:'TodoList',auth:true,
+    // children:[
+    //   {path: "/todoList/123", component: Todo1,name:'Todo1',auth:true},
+    //   {path: "/todoList/456", component: Todo2,name:'Todo2',auth:true},
+    // ]
+  },
 ];
